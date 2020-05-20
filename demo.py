8c3dbc49bf48e4f9198965eeae0bb961f6e87473
ldprita <753041244@qq.com>
2020-05-20 14:50:33 +0800

    pytest第一次实战提交

---
+++ demo.py
@@ -0,0 +1,6 @@
+from pyhton.demo1 import Demo1
+from testing import demo2
+from testing.demo2 import Demo2
+
+print(demo2.hello)
+demo2.f()
---
+++ testing/demo2.py
@@ -0,0 +1,9 @@
+__all__ = ['hello']
+#对外公开的列表或者是类
+
+hello = 'hello demo2'
+def f():
+    print("demo2.py f()")
+
+class Demo2:
+    pass
